387932b049bdcde3cb9ad65cfb9977440bc1b2a8
Rubical <zhpiv96@mail.ru>
2024-12-08 11:49:56 +0300

    test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,36 @@
-import globals from "globals"
+import js from '@eslint/js';
+import globals from 'globals';
+import prettierPlugin from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
 	{
+		plugins: {
+			prettier: prettierPlugin,
+		},
+	},
+		{
+			ignores: ['dist', 'node_modules', 'eslint.config.mjs'],
+		},
+		js.configs.recommended,
+	{
 		languageOptions: {
-			ecmaVersion: 2022,
-			sourceType: "module",
 			globals: {
 				...globals.browser,
 				...globals.node,
-				myCustomGlobal: "readonly"
-			}
+				...globals.es2020,
+			},
 		},
-		
 	},
-]
+	{
+		files: ['**/*.{js}'],
+		rules: {
+			...prettierPlugin.configs.recommended.rules,
+			...eslintConfigPrettier.rules,
+			'prefer-const': 'error',
+			'max-lines': ['warn', { max: 124 }],
+			'max-params': ['error', 3],
+		},
+	},
+];
